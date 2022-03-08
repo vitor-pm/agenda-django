3b70ece0ae31cb382b50b9a82cd9c71d913e43f1
Vitor Melo <vitormelo9913@gmail.com>
2022-03-07 22:27:57 -0300

    TASK: insert into agenda

--- core/views.py
+++ core/views.py
@@ -17,6 +17,24 @@ def logout_user(request):
     return redirect('/')
 
 
+@login_required(login_url='/login')
+def evento(request):
+    return render(request, 'evento.html')
+
+
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
+
+
 def submit_login(request):
     if request.POST:
         username = request.POST.get('username')
@@ -30,10 +48,10 @@ def submit_login(request):
             return redirect('/')
     return redirect('/')
 
+
 @login_required(login_url='/login')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
-
